000fecc4badefc6d43958c1d22af4e6bb9a64095
Justin <you@example.com>
2024-05-06 00:17:15 -0600

    Cambios css

--- tasKing/src/components/App.jsx
+++ tasKing/src/components/App.jsx
@@ -1,6 +1,3 @@
-import { Notification } from './Notification';
-import { SideBar } from './SideBar';
-import Profile from '../assets/imgs/profile.png';
 import { PaginaNotificaciones } from './PaginaNotificaciones';
 
 export function App() {
--- tasKing/src/main.jsx
+++ tasKing/src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Nav } from './components/Nav.jsx'
-import { SideBar } from './components/SideBar.jsx'
-import { Notification } from './components/Notification.jsx'
-import { PaginaNotificaciones } from './components/PaginaNotificaciones.jsx'
 import { App } from './components/App.jsx'
 
 
