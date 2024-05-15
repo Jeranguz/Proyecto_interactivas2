719bc60c4502a5e479f8cee19a417b6d8fd7e78f
francis323 <jose.sequeiramendoza@ucr.ac.cr>
2024-05-15 12:52:25 -0600

    Responsive finalizado

--- tasKing/src/components/DashBoard.jsx
+++ tasKing/src/components/DashBoard.jsx
@@ -9,53 +9,53 @@ import { WorkLoadBento } from './Bento_Componentes/WorkLoadBento';
 export function DashBoard() {
     return (
         <>
-                <div className='w-[70%] sm:w-[75%]  m-auto mt-2'>
-                    <div className=' flex justify-between  m-auto mt-8' >
-                        <h1 className='text-6xl font-bold'>Bienvenido, Jason!</h1>
+            <div className='w-[70%] sm:w-[75%]  m-auto mt-2'>
+                <div className=' flex justify-between  mt-[4vh]' >
+                    <h1 className='text-[10vw] xl:text-[6vh] font-bold'>Bienvenido, Jason!</h1>
 
-                        <div className='flex items-center gap-4'>
+                    <div className='flex items-center gap-4'>
 
-                            <div className='text-end'>
-                                <h2>Jason</h2>
-                                <p>Jason_morales@gmail.com</p>
-                            </div>
-                            <img className='w-[2.688rem] h-[2.688rem]' src={Profile} alt="" />
+                        <div className='xl:block hidden text-end'>
+                            <h2>Jason</h2>
+                            <p>Jason_morales@gmail.com</p>
+                        </div>
+                        <img className='xl:block hidden w-[2.688rem] h-[2.688rem]' src={Profile} alt="" />
+                    </div>
+
+                </div>
+                <div className="grid grid-cols-[repeat(1_,1fr)] grid-rows-[repeat(5_,1fr)] sm:h-[90vh] gap-4 h-[2vh] sm:grid-cols-[repeat(16_,1fr)] sm:gap-[1.5vw]">
+
+
+                    <div className=" max-h-[7300px] space-x-1 w-full h-full sm:bg-textWhite rounded-3xl sm:h-full col-span-1 sm:col-span-7 sm:row-span-3">
+                        <div className='h-[]'>
+                            <Calendario />
                         </div>
 
                     </div>
-                    <div className="grid grid-cols-[repeat(1_,1fr)] grid-rows-[repeat(5_,1fr)] sm:h-[80vh] gap-4 mt-8 h-screen sm:grid-cols-[repeat(16_,1fr)] sm:gap-[2%]">
 
-
-                        <div className=" space-x-1 w-full h-full sm:bg-textWhite rounded-3xl sm:h-full col-span-1 sm:col-span-7 sm:row-span-3">
-                            <div className='h-[]'>
-
+                    <div className="w-full h-full sm:bg-textWhite rounded-3xl sm:h-full sm:col-span-5 sm:row-span-3 ">
+                        <div class="bg-white p-4 rounded-3xl  ">
+                            <h2 class="'text-primary text-[clamp(1rem,_1.5vw,_1.7rem)] font-bold text-center' mb-4">Progreso de cursos</h2>
+                            <div class="mb-2">
+                                <p class="text-sm mb-1">Desarrollo de Aplicaciones Interactivas I</p>
+                                <div class="bg-zinc-200 rounded-full h-2.5">
+                                    <div class="bg-green-500 h-2.5 rounded-full w-[80%]"></div>
+                                </div>
                             </div>
-
+                            <div class="mb-2">
+                                <p class="text-sm mb-1">Diseño Web</p>
+                                <div class="bg-zinc-200 rounded-full h-2.5">
+                                    <div class="bg-yellow-500 h-2.5 rounded-full w-[60%]"></div>
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <p class="text-sm mb-1">Ingeniería de Aplicaciones</p>
+                                <div class="bg-zinc-200 rounded-full h-2.5">
+                                    <div class="bg-red-500 h-2.5 rounded-full w-[30%]"></div>
+                                </div>
+                            </div>
+                            <button class="bg-primary text-white py-2 px-4 rounded-lg w-full hover:bg-purple-700 transition-colors">Ver más</button>
                         </div>
-
-                        <div className="w-full h-full sm:bg-textWhite rounded-3xl sm:h-full sm:col-span-5 sm:row-span-3 ">
-                            <div class="bg-white p-4 rounded-3xl  ">
-                                <h2 class="'text-primary text-[clamp(1rem,_1.5vw,_1.7rem)] font-bold text-center' mb-4">Progreso de cursos</h2>
-                                <div class="mb-2">
-                                    <p class="text-sm mb-1">Desarrollo de Aplicaciones Interactivas I</p>
-                                    <div class="bg-zinc-200 rounded-full h-2.5">
-                                        <div class="bg-green-500 h-2.5 rounded-full w-[80%]"></div>
-                                    </div>
-                                </div>
-                                <div class="mb-2">
-                                    <p class="text-sm mb-1">Diseño Web</p>
-                                    <div class="bg-zinc-200 rounded-full h-2.5">
-                                        <div class="bg-yellow-500 h-2.5 rounded-full w-[60%]"></div>
-                                    </div>
-                                </div>
-                                <div class="mb-4">
-                                    <p class="text-sm mb-1">Ingeniería de Aplicaciones</p>
-                                    <div class="bg-zinc-200 rounded-full h-2.5">
-                                        <div class="bg-red-500 h-2.5 rounded-full w-[30%]"></div>
-                                    </div>
-                                </div>
-                                <button class="bg-primary text-white py-2 px-4 rounded-lg w-full hover:bg-purple-700 transition-colors">Ver más</button>
-                            </div>
                     </div>
 
                     <div className="w-full h-full sm:bg-textWhite rounded-3xl sm:h-full sm:col-start-13 sm:col-span-4 sm:row-span-full">
