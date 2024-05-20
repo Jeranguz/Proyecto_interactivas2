43d2984be902bd34880a5bf78ee41aac7ac5ecfe
Chaves31 <eddychaves.ecr@gmail.com>
2024-05-19 18:24:33 -0600

    Pagina Perfil completa y Comunicados

--- tasKing/src/components/Perfil.jsx
+++ tasKing/src/components/Perfil.jsx
@@ -3,99 +3,163 @@ import Profile from '../assets/imgs/profile.png';
 
 export function Perfil() {
     return (
-        <div class="flex min-h-screen mt-[2rem] gap-4">
-            <div class="grid grid-cols-1 gap-4 w-[100%] h-[100%]">
-                <div class="col-span-1 bg-white p-5 rounded-lg shadow-md">
-                    <div class="flex items-center space-x-4 mb-5">
-                        <img src={Profile} alt="profile picture" class="rounded-full w-[30%]" />
-                        <div>
-                            <h2 class="font-bold text-lg">Jason Morales</h2>
-                            <p>jason.morales@example.com</p>
-                            <p>24 Marzo 1990</p>
 
+        <div>
+
+            {/*Div que divide las secciones en 2 columnas*/}
+            <div class="grid laptop:grid-cols-[repeat(2_,1fr)] md:grid-cols-3 grid-cols-1 gap-4 mt-4 h-[80vh] ">
+
+                {/*Div que contiene las secciones de Datos del Perfil, Cursos Matriculados, Progresos de cursos y el editar datos*/}
+                <div>
+                    
+                    {/* Div que divide en dos columnas los datos del perfil y los cursos matriculados */}
+                    <div className='laptop:w-[46rem] grid grid-cols-2 max-[500px]:grid-cols-1 justify-items-center justify-center'>
+
+                        {/* Datos del usuario */}
+                        <div class="rounded-lg overflow-hidden shadow-lg bg-white p-4 relative h-[13.5rem] mb-[1rem] ">
+                            <div class="flex items-center space-x-4 mt-[2rem]">
+                                <div class="relative">
+                                    <img class='w-[105%] h-[100%] rounded-full border-2 border-purple-500' src={Profile} alt=""/>
+                                    <div class="absolute -bottom-1 -right-1 bg-purple-500 p-2 rounded-full">
+                                        <button className='w-[1.5rem] h-[0.5rem]'> <img src="" alt="" />
+                                        </button>
+                                    </div>
+                                </div>
+                                <div>
+                                    <h2 class="text-lg font-semibold">Jason Morales</h2>
+                                    <p class="text-zinc-600">Jason_morales@gmail.com</p>
+                                    <p class="text-zinc-600">2 de Marzo de 1990</p>
+                                    <p class="text-zinc-600">Costa Rica</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Cursos Matriculados */}
+                        <div class="col-span-1 bg-white p-5 rounded-lg shadow-md mb-[1rem] ml-4 w-[96%] max-[500px]:ml-0 max-[500px]:w-[100%]">
+                            <h3 class="font-bold text-lg mb-4">Cursos matriculados</h3>
+                            <p className='mb-[0.5rem]'>Desarrollo de Aplicaciones Interactivas I</p>
+                            <p className='mb-[0.5rem]'>Diseño Web</p>
+                            <p className='mb-[0.5rem]'>Ingeniería de Aplicaciones</p>
+                            <p className='mb-[0.5rem]'>Ingeniería de Aplicaciones</p>
+                        </div>
+
+
+
+                    </div>
+
+                    
+                    {/* Progreso de Cursos */}
+                    <div class="bg-white p-4 rounded-lg shadow-md w-[100%]">
+                            <h2 class="font-semibold mb-4 text-[clamp(1rem,_2.5vw,_2rem)]" >Progreso de cursos</h2>
+                            <div class="mb-2">
+                                <p class="text-lg mb-1">Desarrollo de Aplicaciones Interactivas I</p>
+                                <div class="bg-zinc-200 rounded-full h-4">
+                                    <div class="bg-green-600 h-4 rounded-full w-[80%]"></div>
+                                </div>
+                            </div>
+                            <div class="mb-2">
+                                <p class="text-lg mb-1">Diseño Web</p>
+                                <div class="bg-zinc-200 rounded-full h-4">
+                                    <div class="bg-yellow-400 h-4 rounded-full w-[60%]"></div>
+                                </div>
+                            </div>
+                            <div class="mb-4">
+                                <p class="text-lg mb-1">Ingeniería de Aplicaciones</p>
+                                <div class="bg-zinc-200 rounded-full h-4">
+                                    <div class="bg-red-600 h-4 rounded-full w-[30%]"></div>
+                                </div>
+                            </div>
+                            <div class="mb-2">
+                                <p class="text-lg mb-1">Desarrollo de Aplicaciones Interactivas I</p>
+                                <div class="bg-zinc-200 rounded-full h-4">
+                                    <div class="bg-green-600 h-4 rounded-full w-[80%]"></div>
+                                </div>
+                            </div>
+                            <button class="bg-purple-600 text-white py-2 px-4 rounded-lg w-[18rem] hover:bg-purple-700 transition-colors mt-[1rem]">Ver más</button>
+                    </div>
+
+                    {/* Editar Datos*/}
+                    <div class="max-w-4xl mx-auto p-5 bg-white shadow-md hidden rounded-lg w-[100%]">
+                        <div class="grid grid-cols-2 gap-4">
+                            <div>
+                            <label for="username" class="block text-lg mb-2">Nombre de Usuario</label>
+                            
+                            <input type="text" id="username" name="username" placeholder="Nombre de Usuario" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                            
+                            <label for="firstname" class="block text-lg mb-2 mt-4">Nombre</label>
+                            <input type="text" id="firstname" name="firstname" placeholder="Nombre" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+
+                            <label for="lastname" class="block text-lg mb-2 mt-4">Apellidos</label>
+                            <input type="text" id="lastname" name="lastname" placeholder="Apellidos" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+
+                            <label for="nationality" class="block text-lg mb-2 mt-4">Nacionalidad</label>
+                            <input type="text" id="nationality" name="nationality" placeholder="Nacionalidad" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                            </div>
+
+                            <div>
+                            <label for="age" class="block text-lg mb-2">Edad</label>
+                            <input type="text" id="age" name="age" placeholder="Edad" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+
+                            <label for="sleep" class="block text-lg mb-2 mt-4">Horas de Sueño</label>
+                            <input type="text" id="sleep" name="sleep" placeholder="Horas de Sueño" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+
+                            <label for="physical" class="block text-lg mb-2 mt-4">Actividad Física Semanal</label>
+                            <input type="text" id="physical" name="physical" placeholder="Actividad Física Semanal" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+
+                            <label for="diseases" class="block text-lg mb-2 mt-4">Enfermedades</label>
+                            <input type="text" id="diseases" name="diseases" placeholder="Enfermedades" class="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                            </div>
+                        </div>
+
+                        <div class="flex justify-between mt-8">
+                            <button class="bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[15rem] max-[500px]:w-[7rem]" type="button">Cancelar</button>
+                            <button class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded focus:outline-none focus:shadow-outline w-[15rem] max-[500px]:w-[7rem]" type="button">Guardar</button>
+                        </div>
+                    </div>
+            
+
+                </div>
+                
+
+                {/* Proximas tareas */}
+                <div class="bg-black shadow-md p-4 rounded-lg w-[100%] h-[100%]">
+                    <h2 class="text-xl font-semibold mb-4 mt-[0.2rem] text-white">Próximos Eventos</h2>
+                    <div class="space-y-4 mb-[1.1rem]">
+                        <div class="bg-primary shadow-lg rounded-lg p-4">
+                            <h3 class="text-lg font-normal text-white">Diseño Web</h3>
+                            <a href="#"><p class="text-white text-sm">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                            
+                        </div>
+                        <div class="bg-primary shadow-lg rounded-lg p-4">
+                            <h3 class="text-lg font-normal text-white">Diseño Web</h3>
+                            <a href="#"><p class="text-white text-sm">
+                                Lorem ipsum dolor sit amet elit.</p>
+                            </a>
+                        </div>
+                        <div class="bg-primary shadow-lg rounded-lg p-4">
+                            <h3 class="text-lg font-normal text-white">Diseño Web</h3>
+                            <a href="#"><p class="text-white text-sm">
+                                Lorem ipsum dolor sit amet elit.</p>
+                            </a>
+                        </div>
+                        <div class="bg-primary shadow-lg rounded-lg p-4">
+                            <h3 class="text-lg font-normal text-white">Diseño Web</h3>
+                            <a href="#"><p class="text-white text-sm">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
+                        </div>
+                        <div class="bg-primary shadow-lg rounded-lg p-4">
+                            <h3 class="text-lg font-normal text-white">Diseño Web</h3>
+                            <a href="#"><p class="text-white text-sm">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </a>
                         </div>
                     </div>
 
-                    <div>
-                        <button class="bg-primary text-white px-3 py-1 rounded mt-2 w-[80%] ml-[2rem] mb-[1rem] mr-[2rem]">Editar datos</button>
-                        <label class="block mb-1 font-semibold">Nombre de Usuario</label>
-                        <input type="text" placeholder="Nombre de Usuario" class="w-full mb-4 p-2 border rounded" />
-                        <label class="block mb-1 font-semibold">Nombre</label>
-                        <input type="text" placeholder="Nombre" class="w-full mb-4 p-2 border rounded" />
-                        <label class="block mb-1 font-semibold">Apellidos</label>
-                        <input type="text" placeholder="Apellidos" class="w-full mb-4 p-2 border rounded" />
-                        <label class="block mb-1 font-semibold">Nacionalidad</label>
-                        <input type="text" placeholder="Nacionalidad" class="w-full p-2 border rounded" />
-                    </div>
-                </div>
+                </div> 
             </div>
-
-            <div className='w-[94%]'>
-                <div class="col-span-1 bg-white p-5 rounded-lg shadow-md mb-[5.2rem]">
-                    <h3 class="font-bold text-lg mb-4">Cursos matriculados</h3>
-                    <p className='mb-[0.5rem]'>Desarrollo de Aplicaciones Interactivas I</p>
-                    <p className='mb-[0.5rem]'>Diseño Web</p>
-                    <p className='mb-[0.5rem]'>Ingeniería de Aplicaciones</p>
-                    <p className='mb-[0.5rem]'>Ingeniería de Aplicaciones</p>
-                </div>
-
-                <div class="bg-white p-4 rounded-lg shadow-md ">
-                    <h2 class="font-semibold mb-4 text-[clamp(1rem,_2.5vw,_2rem)]" >Progreso de cursos</h2>
-                    <div class="mb-2">
-                        <p class="text-sm mb-1">Desarrollo de Aplicaciones Interactivas I</p>
-                        <div class="bg-zinc-200 rounded-full h-2.5">
-                            <div class="bg-green-500 h-2.5 rounded-full w-[80%]"></div>
-                        </div>
-                    </div>
-                    <div class="mb-2">
-                        <p class="text-sm mb-1">Diseño Web</p>
-                        <div class="bg-zinc-200 rounded-full h-2.5">
-                            <div class="bg-yellow-500 h-2.5 rounded-full w-[60%]"></div>
-                        </div>
-                    </div>
-                    <div class="mb-4">
-                        <p class="text-sm mb-1">Ingeniería de Aplicaciones</p>
-                        <div class="bg-zinc-200 rounded-full h-2.5">
-                            <div class="bg-red-500 h-2.5 rounded-full w-[30%]"></div>
-                        </div>
-                    </div>
-                    <button class="bg-purple-600 text-white py-2 px-4 rounded-lg w-full hover:bg-purple-700 transition-colors">Ver más</button>
-                </div>
-            </div>
-
-
-
-            <div class="bg-white shadow-md p-4 rounded-lg w-[85%] h-[100%]">
-                <h2 class="text-xl font-semibold mb-4 mt-[0.2rem]">Próximos Eventos</h2>
-                <div class="space-y-4 mb-[1.1rem]">
-                    <div class="bg-primary shadow-lg rounded-lg p-4">
-                        <h3 class="text-lg font-normal text-white">Diseño Web</h3>
-                        <p class="text-white text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                    <div class="bg-primary shadow-lg rounded-lg p-4">
-                        <h3 class="text-lg font-normal text-white">Diseño Web</h3>
-                        <p class="text-white text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                    <div class="bg-primary shadow-lg rounded-lg p-4">
-                        <h3 class="text-lg font-normal text-white">Diseño Web</h3>
-                        <p class="text-white text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                    <div class="bg-primary shadow-lg rounded-lg p-4">
-                        <h3 class="text-lg font-normal text-white">Diseño Web</h3>
-                        <p class="text-white text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                    <div class="bg-primary shadow-lg rounded-lg p-4">
-                        <h3 class="text-lg font-normal text-white">Diseño Web</h3>
-                        <p class="text-white text-sm">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </div>
-                </div>
-
-            </div>
-        </div>
+        </div> 
     );
 }
